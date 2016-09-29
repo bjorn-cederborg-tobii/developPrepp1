6c5e3ab1b7ec4cd0e09f5b41c6140144f05fd22d
Bjorn Cederborg (bcg) <bjorn.cederborg@tobii.com>
2016-09-29 15:48:32 +0200

    added server and client

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter s for server");
+            if (Console.ReadLine() == "s")
+            {
+                Console.WriteLine("Starting echo server...");
+                StartServer();
+            }
+            else
+            {
+                Console.WriteLine("Starting client...");
+                StartClient();
+            }
+
+        }
+
+        static void StartServer()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Parse("0.0.0.0"), 1234);
+            listener.Start();
+            var client = listener.AcceptTcpClientAsync();
+            client.Wait();
+
+            var reader = new StreamReader(client.Result.GetStream(), Encoding.ASCII);
+
+            while (true)
+            {
+                Console.WriteLine(reader.ReadLine());
+            }
+        }
+
+        static void StartClient()
+        {
+            TcpClient client = new TcpClient();
+            client.ConnectAsync("localhost", 1234).Wait();
+            StreamWriter writer = new StreamWriter(client.GetStream()) { AutoFlush = true };
+
+            while (true)
+            {
+                writer.WriteLine(Console.ReadLine());
+            }
+        }
+    }
+}
